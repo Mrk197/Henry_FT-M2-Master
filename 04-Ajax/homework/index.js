277f527dc6434d94ed9d8214920b8ecf5e924d5b
Mrk197 <mirkaalamilla97@gmail.com>
2022-09-19 19:24:53 -0500

    Homework 04 Review

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,4 +1,4 @@
-
+/*
 $('#boton').click(function () {
     $.get("http://localhost:5000/amigos", function (respuesta) {
         document.getElementById("lista").innerHTML = "";
@@ -9,9 +9,21 @@ $('#boton').click(function () {
             $("#lista").append(amigos);
         }
     });
+});*/
+
+$('#boton').click(() =>{
+    //tomo el elemento que tiene la lista 
+    let list = $('#lista');
+    list.empty();
+    $.get('http://localhost:5000/amigos', (data)=>{
+        //recorrer el array de objetos
+        for (const datos of data){
+            list.append(`<li>${datos.name}</li>`);
+        } 
+    });
 });
 
-$('#search').click(function () {
+/*$('#search').click(function () {
     let input = document.getElementById('input').value;
     document.getElementById('amigo').innerHTML = "";
     //alert(input.value);
@@ -24,9 +36,21 @@ $('#search').click(function () {
             $("#amigo").append(res.name);
         }
     });
+});*/
+
+$('#search').click(() => {
+    let searchID = $('#input').val();
+    //hacer un request con ese valor 
+    $.get('http://localhost:5000/amigos/'+searchID, (data)=>{
+        if (data) {
+            
+        }
+        $('#amigo').text(data.name); //se usa text porque sólo se guarda texto y no un elemento
+    });
 });
 
-$('#delete').click(function () {
+
+/*$('#delete').click(function () {
     let input = document.getElementById('inputDelete').value;
     alert(input);
     $.ajax({
@@ -34,6 +58,26 @@ $('#delete').click(function () {
         type: 'delete',
         success:function (res) {
             $("#success").append("Tu amigo fue borrado con éxito");
+        }
+    });
+});*/
+
+$('#delete').click(() => {
+    //guardamos el valor del input
+    let deleteID = $('#inputDelete').val();
+    $.ajax({
+        url:'http://localhost:5000/amigos/'+deleteID,
+        type: 'delete',
+        success:(data) => {
+            $("#success").text("Tu amigo fue borrado con éxito");
+
+            //re denderizar lista
+            let list = $('#lista');
+            list.empty();
+            for (const datos of data){
+                list.append(`<li>${datos.name}</li>`);
+            }
+            //or $('boton').click();
         }
     });
 });
