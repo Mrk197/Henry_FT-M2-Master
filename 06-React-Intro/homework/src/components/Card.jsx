9964f00160dbcee8e09419852498bb1f4258bd18
Mrk197 <mirkaalamilla97.rk@gmail.com>
2022-09-26 07:34:28 -0500

    Homework 06

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,31 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  //destructuring de props
+  const { max, min, name, img, onClose } = props;
+
+  const MaxComponent = <div>
+    <p>Max</p>
+    <p>{max}</p>
+  </div>
+
+  const MinComponent = <div>
+    <p>Min</p>
+    <p>{min}</p>
+  </div>
+
+
+  return <div>
+    <button onClick={onClose}>x</button>
+    <h4>{name}</h4>
+    <div>
+      <div>
+        {MinComponent}
+        {MaxComponent}
+      </div>
+      <div>
+        <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt={img} />
+      </div>
+    </div>
+  </div>
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return <div>
+    <input type="text" name='ciudad...'></input>
+    <input type="submit" value="Agregar" onClick={props.onSearch}></input>
+  </div>
 };
