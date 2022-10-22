9f7cf03024739bb0221e3ee7aa0098dba88b2cca
Mrk197 <mirkaalamilla97@gmail.com>
2022-10-21 19:29:25 -0500

    Extra

--- EjerciciosExtras/homework/src/actions/index.js
+++ EjerciciosExtras/homework/src/actions/index.js
@@ -1,5 +1,67 @@
-export const GET_ALL_POSTS = '';
-export const GET_ALL_USERS = '';
-export const GET_ALL_USERS_POST = '';
-export const GET_ALL_COMMENTS_POST = '';
+export const GET_ALL_POSTS = 'GET_ALL_POSTS';
+export const GET_ALL_USERS = 'GET_ALL_USERS';
+export const GET_ALL_USERS_POST = 'GET_ALL_USERS_POST';
+export const GET_ALL_COMMENTS_POST = 'GET_ALL_COMMENTS_POST';
 
+//action creators 
+//asyncrono
+export function getAllUsers(){
+    return function(dispatch) { //retorna función para hacer la petición cuando se ejecute la acción 
+        return fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json()) //captura respuesta y convierte a JSON (OBJ JS)
+        .then(
+            //obtiene OBJ JS y envía acción
+            posts => {
+                dispatch({
+                    type: GET_ALL_USERS,
+                    payload: posts
+                })
+            }
+        );  
+    }
+}
+
+export function getAllUserPosts(id){
+    return function (dispatch) {
+        return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+        .then(response => response.json()) 
+        .then(
+            posts => {
+                dispatch({
+                    type: GET_ALL_USERS_POST,
+                    payload: posts
+                })
+            }
+        )
+    }
+}
+
+export function getAllCommentsPost(id){
+    return function(dispatch){
+        return fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
+        .then(response => response.json())
+        .then(
+            posts => {
+                dispatch({
+                    type: GET_ALL_COMMENTS_POST,
+                    payload: posts
+                })
+            }
+        )
+    }
+}
+
+export function getAllPosts(){
+    return function(dispatch){
+        return fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(
+            posts => {
+                dispatch({
+                    type: GET_ALL_POSTS,
+                    payload: posts
+                })
+            }
+        )
+    }
+}
---
+++ EjerciciosExtras/newApp/repasoapp/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+    return(
+    <nav>
+        <a>Menu1</a>
+        <a>Menu2</a>
+        <a>Menu3</a>
+    </nav>
+    );
+}
+
+export default NavBar;
